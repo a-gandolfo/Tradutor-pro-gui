a3ef1e9735a52e7094fcdcd70766c8c911116f35
a-gandolfo <125313362+a-gandolfo@users.noreply.github.com>
2023-08-27 20:50:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const textAreaFrom = document.querySelector('#textAreaFrom');
+const textAreaTo = document.querySelector('#textAreaTo');
+const btnTranslate = document.querySelector('#btnTranslate')
+const selects = document.querySelectorAll('select');
+
+const countries = {
+    "en-GB": "Inglês",
+    "es-ES": "Espanhol",
+    "it-IT": "Italiano",
+    "ja-JP": "Japonês",
+    "pt-BR": "Português"
+};
+
+selects.forEach((tag) => {
+    for(let country in countries){
+        let selected;
+        if(tag.className.includes("selectFrom") && country === 'pt-BR'){
+            selected = "selected";
+        } else if(tag.className.includes('selectTo') && country === 'pt-BR'){
+            selected = "selected";
+        }
+
+        const option = `<option value="${country}" ${selected}>${countries[country]}</option>`;
+        tag.insertAdjacentHTML('beforeend', option);
+    }
+});
+
+btnTranslate.addEventListener('click', () => {
+    if(textAreaFrom.value){
+        loadTranslation()
+    }else{
+        textAreaTo.value = '';
+    }
+})
+
+function loadTranslation(){
+    fetch(
+        `https://api.mymemory.translated.net/get?q=${textAreaFrom.value}&langpair=${selects[0].value}|${selects[1].value}`
+    ).then((res) => res.json()).then((data => {
+        textAreaTo.value = data.responseData.translatedText;
+    }))
+}
